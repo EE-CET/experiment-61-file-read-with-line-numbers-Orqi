964cadb776d3f38a8b7e50360a4c75e509ef11a4
Amrita Reji <amritareji@Amritas-MacBook-Pro.local>
2026-02-20 09:49:30 +0530

    hi

--- src/FileLineReader.java
+++ src/FileLineReader.java
@@ -3,15 +3,18 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class FileLineReader {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
         String fileName = "input.txt";
         
-        // TODO: Use a try-with-resources block to open a BufferedReader for the file
-        // TODO: Read the file line by line using a loop
-        // TODO: Keep track of the line number (starting from 1)
-        // TODO: Print each line in the format "lineNumber: lineContent"
-        // TODO: Catch and handle any IOExceptions that might occur
+        int count=1;
+        try(BufferedReader br =new BufferedReader(new FileReader("input.txt"))){
+                String line;
+                while((line=br.readLine())!=null){
+                        System.out.println(count+ ": "+line);
+                        count++;
+                }
+
         
         
     }
-}
+}}
